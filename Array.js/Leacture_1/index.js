1bd369cef13a624bd4ef15915e51483a4dd00bc5
chetanbnagmoti <cbn9860@gmail.com>
2024-02-25 19:55:28 +0530

    2/25/2024

---
+++ Array.js/Leacture_1/index.js
@@ -0,0 +1,11 @@
+const arr=[1,2,3,"Chetan"];
+arr.push(4);
+arr.unshift(0);
+arr.pop();
+arr.shift();
+
+for(const e of arr){
+    console.log(e);
+}
+
+//map ,filter,reduce,concat,slice,splice method:-
---
+++ Leacture-2/Object/index.js
@@ -0,0 +1,12 @@
+const obj={
+    name:'Chetan',
+    'age':25,
+    keythree:true,
+    'key-three':false
+}
+
+console.log(obj);
+console.log(obj['age']);
+console.log(obj.age);
+console.log(obj.keythree);
+console.log(obj["key-three"])
